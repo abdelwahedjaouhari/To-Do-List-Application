8d4632ec3ee0cf02d93932c929ad11f9f2d6868b
abdelwahedjaouhari <abdelwahedjaouhari@gmail.com>
2024-02-03 01:51:01 +0100

    part 6

---
+++ new_tasks.py
@@ -0,0 +1,11 @@
+
+tasks = [{"task":"Quran", "completed":True}, {"task":"Salah", "completed":True}, 
+         {"task":"Study", "completed":False}, {"task":"Exercise", "completed":True}, 
+         {"task":"Sleep", "completed":False}, {"task":"Visit Hamada", "completed":True}]
+
+
+
+
+
+
+
